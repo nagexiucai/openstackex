6422079b224a04e80ce20a7766a086169efd16b7
bob-nx <me@nagexiucai.com>
2017-10-31 11:26:37 +0800

    omit compose.py

---
+++ core/compose.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @Time    : 2017/10/31 10:57
+# @Author  : Bob
+# @Website : www.nagexiucai.com
+# @E-Mail  : me@nagexiucai.com
+# @Summary : Orchestration based on heat, PythonIII
+
+from heatclient.client import Client as HeatClient
+
+hc = HeatClient()
